fd420bbcc1412dd36e2410026d39b0d87d9c67ae
7RedViolin <66324633+7RedViolin@users.noreply.github.com>
2023-04-21 16:07:32 -0500

    remove unnecessary comments

--- sigma/pipelines/sentinelonedeepvisibility/__init__.py
+++ sigma/pipelines/sentinelonedeepvisibility/__init__.py
@@ -1,5 +1,4 @@
 from .sentinelonedeepvisibility import sentinelonedeepvisibility_pipeline
-# TODO: add all pipelines that should be exposed to the user of your backend in the import statement above.
 
 pipelines = {
     "sentinelonedeepvisibility_pipeline": sentinelonedeepvisibility_pipeline,   # TODO: adapt identifier to something approproiate
