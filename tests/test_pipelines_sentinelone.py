cfb5faf17eddce421180a6bec51bdb1d2a031aee
7RedViolin <66324633+7RedViolin@users.noreply.github.com>
2023-04-22 22:49:43 -0500

    add dns and network_connection types

--- tests/test_pipelines_sentinelone.py
+++ tests/test_pipelines_sentinelone.py
@@ -19,7 +19,7 @@ def test_sentinelone_windows_os_filter(sentinelone_backend : SentinelOneBackend)
                     Image: valueA
                 condition: sel
         """)
-    ) == ['EventType="Process Creation" AND (EndpointOS="windows" AND TgtProcName="valueA")']
+    ) == ['EventType = "Process Creation" AND (EndpointOS = "windows" AND TgtProcName = "valueA")']
 
 def test_sentinelone_linux_os_filter(sentinelone_backend : SentinelOneBackend):
     assert sentinelone_backend.convert(
@@ -34,7 +34,22 @@ def test_sentinelone_linux_os_filter(sentinelone_backend : SentinelOneBackend):
                     Image: valueA
                 condition: sel
         """)
-    ) == ['EventType="Process Creation" AND (EndpointOS="linux" AND TgtProcName="valueA")']
+    ) == ['EventType = "Process Creation" AND (EndpointOS = "linux" AND TgtProcName = "valueA")']
+
+def test_sentinelone_osx_os_filter(sentinelone_backend : SentinelOneBackend):
+    assert sentinelone_backend.convert(
+        SigmaCollection.from_yaml("""
+            title: Test
+            status: test
+            logsource:
+                category: process_creation
+                product: macos
+            detection:
+                sel:
+                    Image: valueA
+                condition: sel
+        """)
+    ) == ['EventType = "Process Creation" AND (EndpointOS = "osx" AND TgtProcName = "valueA")']
 
 def test_sentinelone_process_creation_mapping(sentinelone_backend : SentinelOneBackend):
     assert sentinelone_backend.convert(
@@ -64,12 +79,12 @@ def test_sentinelone_process_creation_mapping(sentinelone_backend : SentinelOneB
                     ParentCommandLine: Get-Path
                 condition: sel
         """)
-    ) == ['EventType="Process Creation" AND (TgtProcPID=12 AND TgtProcName="valueA" AND TgtProcDisplayName="foo bar" AND ' +
-          'TgtProcDisplayName="bar foo" AND TgtProcPublisher="foo foo" AND TgtProcCmdLine="invoke-mimikatz" AND ' +
-          'TgtProcImagePath="/etc" AND TgtProcUser="administrator" AND TgtProcSessionId=4 AND ' +
-          'TgtProcIntegrityLevel="bar bar" AND TgtProcMd5="asdfasdfasdfasdfasdf" AND ' + 
-          'TgtProcSha1="asdfasdfasdfasdfasdfasdf" AND TgtProcSha256="asdfasdfasdfasdfasdfasdfasdfasdf" AND SrcProcPID=13 AND ' +
-          'SrcProcName="valueB" AND SrcProcCmdLine="Get-Path")']
+    ) == ['EventType = "Process Creation" AND (TgtProcPID = "12" AND TgtProcName = "valueA" AND TgtProcDisplayName = "foo bar" AND ' +
+          'TgtProcDisplayName = "bar foo" AND TgtProcPublisher = "foo foo" AND TgtProcCmdLine = "invoke-mimikatz" AND ' +
+          'TgtProcImagePath = "/etc" AND TgtProcUser = "administrator" AND TgtProcSessionId = "4" AND ' +
+          'TgtProcIntegrityLevel = "bar bar" AND TgtProcMd5 = "asdfasdfasdfasdfasdf" AND ' + 
+          'TgtProcSha1 = "asdfasdfasdfasdfasdfasdf" AND TgtProcSha256 = "asdfasdfasdfasdfasdfasdfasdfasdf" AND SrcProcPID = "13" AND ' +
+          'SrcProcName = "valueB" AND SrcProcCmdLine = "Get-Path")']
 
 def test_sentinelone_file_mapping(sentinelone_backend : SentinelOneBackend):
     assert sentinelone_backend.convert(
@@ -90,9 +105,9 @@ def test_sentinelone_file_mapping(sentinelone_backend : SentinelOneBackend):
                     User: administrator
                 condition: sel
         """)
-    ) == ['ObjectType="File" AND (SrcProcName="valueA" AND SrcProcCmdLine="invoke-mimikatz" AND ' +
-          'SrcProcParentName="valueB" AND SrcProcParentCmdline="Get-Path" AND TgtFilePath="foo bar" AND ' + 
-          'TgtFileOldPath="bar foo" AND SrcProcUser="administrator")']
+    ) == ['ObjectType = "File" AND (SrcProcName = "valueA" AND SrcProcCmdLine = "invoke-mimikatz" AND ' +
+          'SrcProcParentName = "valueB" AND SrcProcParentCmdline = "Get-Path" AND TgtFilePath = "foo bar" AND ' + 
+          'TgtFileOldPath = "bar foo" AND SrcProcUser = "administrator")']
 
 def test_sentinelone_image_load_mapping(sentinelone_backend : SentinelOneBackend):
     assert sentinelone_backend.convert(
@@ -113,9 +128,9 @@ def test_sentinelone_image_load_mapping(sentinelone_backend : SentinelOneBackend
                     ImageLoaded: foo bar
                 condition: sel
         """)
-    ) == ['EventType="ModuleLoad" AND (SrcProcName="valueA" AND SrcProcCmdLine="invoke-mimikatz" AND ' +
-          'SrcProcParentName="valueB" AND SrcProcParentCmdline="Get-Path" AND ModuleSha1="asdfasdf" AND ' + 
-          'ModuleMd5="asdfasdfasdf" AND ModulePath="foo bar")']
+    ) == ['EventType = "ModuleLoad" AND (SrcProcName = "valueA" AND SrcProcCmdLine = "invoke-mimikatz" AND ' +
+          'SrcProcParentName = "valueB" AND SrcProcParentCmdline = "Get-Path" AND ModuleSha1 = "asdfasdf" AND ' + 
+          'ModuleMd5 = "asdfasdfasdf" AND ModulePath = "foo bar")']
 
 def test_sentinelone_pipe_creation_mapping(sentinelone_backend : SentinelOneBackend):
     assert sentinelone_backend.convert(
@@ -134,8 +149,8 @@ def test_sentinelone_pipe_creation_mapping(sentinelone_backend : SentinelOneBack
                     PipeName: foo bar
                 condition: sel
         """)
-    ) == ['EventType="Named Pipe Creation" AND (SrcProcName="valueA" AND SrcProcCmdLine="invoke-mimikatz" AND ' +
-          'SrcProcParentName="valueB" AND SrcProcParentCmdline="Get-Path" AND NamedPipeName="foo bar")']
+    ) == ['EventType = "Named Pipe Creation" AND (SrcProcName = "valueA" AND SrcProcCmdLine = "invoke-mimikatz" AND ' +
+          'SrcProcParentName = "valueB" AND SrcProcParentCmdline = "Get-Path" AND NamedPipeName = "foo bar")']
 
 def test_sentinelone_registry_mapping(sentinelone_backend : SentinelOneBackend):
     assert sentinelone_backend.convert(
@@ -155,9 +170,65 @@ def test_sentinelone_registry_mapping(sentinelone_backend : SentinelOneBackend):
                     Details: bar foo
                 condition: sel
         """)
-    ) == ['ObjectType="Registry" AND (SrcProcName="valueA" AND SrcProcCmdLine="invoke-mimikatz" AND ' +
-          'SrcProcParentName="valueB" AND SrcProcParentCmdline="Get-Path" AND RegistryKeyPath="foo bar" AND ' + 
-          'RegistryValue="bar foo")']
+    ) == ['ObjectType = "Registry" AND (SrcProcName = "valueA" AND SrcProcCmdLine = "invoke-mimikatz" AND ' +
+          'SrcProcParentName = "valueB" AND SrcProcParentCmdline = "Get-Path" AND RegistryKeyPath = "foo bar" AND ' + 
+          'RegistryValue = "bar foo")']
+
+def test_sentinelone_dns_mapping(sentinelone_backend : SentinelOneBackend):
+    assert sentinelone_backend.convert(
+        SigmaCollection.from_yaml("""
+            title: Test
+            status: test
+            logsource:
+                category: dns
+                product: test_product
+            detection:
+                sel:
+                    Image: valueA
+                    CommandLine: invoke-mimikatz
+                    ParentImage: valueB
+                    ParentCommandLine: Get-Path
+                    query: foo bar
+                    answer: bar foo
+                    QueryName: foo foo
+                    record_type: bar bar
+                condition: sel
+        """)
+    ) == ['ObjectType = "DNS" AND (SrcProcName = "valueA" AND SrcProcCmdLine = "invoke-mimikatz" AND ' +
+          'SrcProcParentName = "valueB" AND SrcProcParentCmdline = "Get-Path" AND DnsRequest = "foo bar" AND ' + 
+          'DnsResponse = "bar foo" AND DnsRequest = "foo foo" AND DnsResponse = "bar bar")']
+
+def test_sentinelone_network_mapping(sentinelone_backend : SentinelOneBackend):
+    assert sentinelone_backend.convert(
+        SigmaCollection.from_yaml("""
+            title: Test
+            status: test
+            logsource:
+                category: network_connection
+                product: test_product
+            detection:
+                sel:
+                    Image: valueA
+                    CommandLine: invoke-mimikatz
+                    ParentImage: valueB
+                    ParentCommandLine: Get-Path
+                    DestinationHostname: foo bar
+                    DestinationPort: 445
+                    DestinationIp: 0.0.0.0
+                    User: administrator
+                    SourceIp: 1.1.1.1
+                    SourcePort: 135
+                    Protocol: udp
+                    dst_ip: 2.2.2.2
+                    src_ip: 3.3.3.3
+                    dst_port: 80
+                    src_port: 8080
+                condition: sel
+        """)
+    ) == ['(ObjectType In ("DNS","Url","IP")) AND (SrcProcName = "valueA" AND SrcProcCmdLine = "invoke-mimikatz" AND ' + 
+          'SrcProcParentName = "valueB" AND SrcProcParentCmdline = "Get-Path" AND (Url = "foo bar" OR DnsRequest = "foo bar") AND ' + 
+          'DstPort = "445" AND DstIP = "0.0.0.0" AND SrcProcUser = "administrator" AND SrcIP = "1.1.1.1" AND SrcPort = "135" AND ' + 
+          'NetProtocolName = "udp" AND DstIP = "2.2.2.2" AND SrcIP = "3.3.3.3" AND DstPort = "80" AND SrcPort = "8080")']
 
 def test_sentinelone_unsupported_rule_type(sentinelone_backend : SentinelOneBackend):
   with pytest.raises(ValueError):
